8d4c9e9ebd084c08e13ccfb7b4185bf66c301a62
yoohasoom <yhb01037289920@gmail.com>
2023-11-15 11:28:19 +0900

    Env

---
+++ myfile/src/main/java/test/Env.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class Env {
+
+        public static void main(String[] args) {
+                printProps();
+                printEnv();
+                System.out.println(System.getProperty("user.dir"));
+                System.out.println(System.getProperty("sonar.token"));
+                System.out.println(System.getenv("SONAR_TOKEN"));
+                System.out.println("---");
+        }
+
+        private static void printEnv() {
+                Map<String, String> map = System.getenv();
+                for (Map.Entry<String, String> entry: map.entrySet()) {
+                        System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
+        }
+
+        public static void printProps() {
+                Properties properties = System.getProperties();
+                for (String key : properties.stringPropertyNames()) {
+                        String value = properties.getProperty(key, "");
+                        System.out.println(key + ": " + value);
+                }
+        }
+
+}
